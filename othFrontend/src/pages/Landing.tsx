e5180ed7475b57d5a1420816fd99b9edca02db0e
Anders <andyret26@gmail.com>
2023-11-01 16:57:45 +0100

    Removed osu-web.js

--- othFrontend/src/pages/Landing.tsx
+++ othFrontend/src/pages/Landing.tsx
@@ -1,14 +1,3 @@
-import { Test } from "../services/osuApiService"
-
 export default function Landing() {
-  const handleClick = () => {
-    Test()
-  }
-  return (
-    <div>
-      <button type="button" onClick={() => handleClick()}>
-        CLick
-      </button>
-    </div>
-  )
+  return <div>Landing!</div>
 }
--- othFrontend/src/services/osuApiService.ts
+++ othFrontend/src/services/osuApiService.ts
@@ -1,14 +1,8 @@
 import axios from "axios"
 
-import { Auth } from "osu-web.js"
 import { OsuUserData } from "../helpers/interfaces"
 
 const API_KEY = import.meta.env.VITE_OSU_API_KEY
-const AUTH = new Auth(
-  1, // Clietn id here
-  "Secret Here",
-  "http://localhost:5173/"
-)
 
 export async function getUserDataAsync(id: string): Promise<OsuUserData[]> {
   const response = await axios.get("https://osu.ppy.sh/api/v1/get_user", {
@@ -21,10 +15,4 @@ export async function getUserDataAsync(id: string): Promise<OsuUserData[]> {
   })
 
   return response.data
-}
-
-export async function Test(): Promise<void> {
-  console.log("In test osuApiService")
-  const test = await AUTH.clientCredentialsGrant()
-  console.log(test)
 }
