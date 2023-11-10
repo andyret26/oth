3bcf8ac372a5b73fee2951363b488e0934e72f23
Anders <andyret26@gmail.com>
2023-11-10 14:46:07 +0100

    updated

--- othFrontend/src/components/Search.tsx
+++ othFrontend/src/components/Search.tsx
@@ -17,7 +17,7 @@ const Search = () => {
       setPlayers(await GetPlayersMinAsync())
     }
     fetchPlayers()
-  }, [players])
+  }, [])
 
   function handleSearch(searchtest: string): void {
     if (!players) {
