cb313e55105028fa535279a33ec22356a5af20e7
andyret26 <andyret26@gmail.com>
2023-11-10 09:50:48 +0100

    Fixed warnings, added tab icon

--- othFrontend/vite.config.ts
+++ othFrontend/vite.config.ts
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), {
+    // Use a Vite plugin to configure the server
+    name: 'custom-middleware',
+    configureServer({ middlewares }) {
+      middlewares.use((req, res, next) => {
+        // Set the Permissions-Policy header to disable specific features
+        res.setHeader('Permissions-Policy', 'accelerometer=(), camera=()');
+        next();
+      });
+    },
+  },],
+
 })
