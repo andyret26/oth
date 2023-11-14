33168a7d21e0f8e56298b6dea3a9383240e6b4f2
Anders <andyret26@gmail.com>
2023-11-14 15:10:12 +0100

    Loding when startingup

--- othFrontend/src/App.tsx
+++ othFrontend/src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import "./App.css"
 import { LocalizationProvider } from "@mui/x-date-pickers"
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs"
+import { useEffect, useState } from "react"
+import { CircularProgress } from "@mui/material"
 import NavBar from "./components/NavBar"
 import History from "./pages/History"
 import Landing from "./pages/Landing"
@@ -10,35 +12,54 @@ import EditTournament from "./pages/EditTournament"
 import Unauthorized from "./pages/Unauthorized"
 import { PrivateRoute } from "./components/PrivateRoute"
 import NotFound from "./pages/NotFound"
+import { PlayerMin } from "./helpers/interfaces"
+import { GetPlayersMinAsync } from "./services/othApiService"
 
 function App() {
+  const [isRunning, setIsRunnning] = useState<PlayerMin[] | null>(null)
+  useEffect(() => {
+    GetPlayersMinAsync().then((res: PlayerMin[]) => {
+      setIsRunnning(res)
+    })
+  }, [])
+
   return (
     <div>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <BrowserRouter>
-          <NavBar />
-          <Routes>
-            <Route path="/" Component={Landing} />
-            <Route path="/history/:id" Component={History} />
-            <Route
-              path="/tournament/create"
-              element={
-                <PrivateRoute>
-                  <CreateTournament />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/tournament/edit/:id"
-              element={
-                <PrivateRoute>
-                  <EditTournament />
-                </PrivateRoute>
-              }
-            />
-            <Route path="/unauthorized" Component={Unauthorized} />
-            <Route path="*" Component={NotFound} />
-          </Routes>
+          {isRunning === null ? (
+            <>
+              <CircularProgress />
+              <p>Starting up server pls wait...</p>
+              <p>If it takes to long, refresh page</p>
+            </>
+          ) : (
+            <>
+              <NavBar />
+              <Routes>
+                <Route path="/" Component={Landing} />
+                <Route path="/history/:id" Component={History} />
+                <Route
+                  path="/tournament/create"
+                  element={
+                    <PrivateRoute>
+                      <CreateTournament />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/tournament/edit/:id"
+                  element={
+                    <PrivateRoute>
+                      <EditTournament />
+                    </PrivateRoute>
+                  }
+                />
+                <Route path="/unauthorized" Component={Unauthorized} />
+                <Route path="*" Component={NotFound} />
+              </Routes>
+            </>
+          )}
         </BrowserRouter>
       </LocalizationProvider>
     </div>
