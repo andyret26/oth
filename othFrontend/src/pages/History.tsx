305cf04324fea74f69a954481cf44585f734ed03
andyret26 <andyret26@gmail.com>
2023-11-15 10:49:13 +0100

    removed unused import

--- othFrontend/src/pages/History.tsx
+++ othFrontend/src/pages/History.tsx
@@ -1,7 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react"
 import { useEffect, useState } from "react"
 import dayjs from "dayjs"
-import axios from "axios"
 import CircularProgress from "@mui/material/CircularProgress"
 import { useLocation } from "react-router-dom"
 import { GetTournamentsByPlayerIdAsync } from "../services/othApiService"
