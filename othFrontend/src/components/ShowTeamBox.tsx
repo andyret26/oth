46ed8142adb4089f97c28e06990c1a223e97339e
andyret26 <andyret26@gmail.com>
2024-02-07 14:20:07 +0100

    :bug: fix flickering when hover over team_name to show team

--- othFrontend/src/components/ShowTeamBox.tsx
+++ othFrontend/src/components/ShowTeamBox.tsx
@@ -7,7 +7,7 @@ export default function ShowTeamBox({ t }: CompProps) {
   return (
     <div
       id="team-mates"
-      className="z-50 bg-[#4e3c44]/95 flex flex-wrap gap-x-7 gap-y-2 justify-center w-72 absolute top-[7.5rem] rounded-md p-2 animate__animated animate__fadeIn animate__faster"
+      className="z-50 bg-[#4e3c44]/95 flex flex-wrap gap-x-7 gap-y-2 justify-center w-72 absolute top-[120px] rounded-md p-2 animate__animated animate__fadeIn animate__faster"
     >
       {t.teamMates.map((tm) => (
         <div key={tm.id} className="flex items-center gap-1">
