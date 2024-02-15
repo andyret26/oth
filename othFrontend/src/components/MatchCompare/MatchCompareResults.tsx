1e1e70314ad11eceb1f6b7cf82d5e1b8fc359522
andyret26 <andyret26@gmail.com>
2024-02-15 11:54:15 +0100

    :ribbon: Match compare styling and mod images

--- othFrontend/src/components/MatchCompare/MatchCompareResults.tsx
+++ othFrontend/src/components/MatchCompare/MatchCompareResults.tsx
@@ -9,20 +9,31 @@ interface CompProps {
 export default function MatchCompareResults({ onReset, maps }: CompProps) {
   return (
     <div className="results">
-      <Button onClick={onReset} variant="outlined">
+      <Button className="results__btn-new" onClick={onReset} variant="outlined">
         New Compare
       </Button>
       <div className="results__maps-header">
         <p className="results__header-field results__header-title">Title</p>
         <p className="results__header-field results__header-score1">Score1</p>
         <p className="results__header-field results__header-diff">Diff</p>
-        <p className="results__header-field results__headerscore2">Score2</p>
+        <p className="results__header-field results__header-score2">Score2</p>
       </div>
       <div className="results__maps-container">
         {maps.map((map) => (
           <div className="results__map" key={map.score2}>
             <h3 className="results__map-field results__map-title ">
               {map.title}
+              <div className="results__icons ">
+                {map.mods
+                  .filter((m) => m !== "NF")
+                  .map((mod) => (
+                    <img
+                      className="w-8"
+                      src={`src/assets/mod_${mod}.png`}
+                      alt="test"
+                    />
+                  ))}
+              </div>
             </h3>
             <p className="results__map-field results__map-score1">
               {map.score1}
