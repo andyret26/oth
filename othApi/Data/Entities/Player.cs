0d5dbfa9d8d70b2a74c307cad3ee66d792a02f26
andyret26 <andyret26@gmail.com>
2024-03-11 09:49:55 +0100

    Migrations add Discord Tag

--- othApi/Data/Entities/Player.cs
+++ othApi/Data/Entities/Player.cs
@@ -5,6 +5,7 @@ public class Player
 {
     public int Id { get; set; }
     public string Username { get; set; } = null!;
+    public string? DiscordTag { get; set; }
     public string Avatar_url { get; set; } = null!;
     public int Global_rank { get; set; }
     public string Country_code { get; set; } = null!;
