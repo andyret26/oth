35976fcc65834c740f604c054c870777288ad41c
andyret26 <andyret26@gmail.com>
2024-04-02 13:49:02 +0200

    :wrench: Updated HostController
    Updated post endpoint with the new ErrorResponse class.
    added Authorization.
    Proper Response type status codes

--- othApi/Controllers/Otm/HostController.cs
+++ othApi/Controllers/Otm/HostController.cs
@@ -1,8 +1,11 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using othApi.Data.Entities.Otm;
 using othApi.Services.OsuApi;
 using othApi.Services.Otm.HostService;
 using othApi.Services.Players;
+using othApi.Utils;
 
 namespace othApi.Controllers.Otm;
 
@@ -29,10 +32,19 @@ public class HostController : ControllerBase
     }
 
     [HttpPost("{id}")]
-    [ProducesResponseType(200)]
-    [ProducesResponseType(404)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status409Conflict)]
+    [Authorize]
     public async Task<ActionResult> PostHost(int id)
     {
+        var tokenSub = User.FindFirst(ClaimTypes.NameIdentifier);
+        if (tokenSub == null) return Unauthorized(new ErrorResponse("Unauthorized", 401, "Invalid token"));
+        if (tokenSub.Value.Split("|")[2] != id.ToString()) return Unauthorized(new ErrorResponse("Unauthorized", 401, "Token does not match id"));
+
+        if (await _hostService.ExistsAsync(id)) return Conflict(new ErrorResponse("Conflict", 409, "Host already exists"));
+
         try
         {
 
