df7524a2b2e554444ddbf85f26331e4060ce635b
Anders <andyret26@gmail.com>
2025-10-12 00:03:06 +0200

    Maybe this works

--- othFrontend/src/pages/MatchCompare/MatchCompare.tsx
+++ othFrontend/src/pages/MatchCompare/MatchCompare.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react"
-import MatchCompareForm from "../../components/MatchCompare/MatchCompareForm"
-import MatchCompareResults from "../../components/MatchCompare/MatchCompareResults"
+import MatchCompareForm from "../../components/MatchCompare/MatchCompareForm.tsx"
+import MatchCompareResults from "../../components/MatchCompare/MatchCompareResults.tsx"
 import "./MatchCompare.scss"
-import { MapV1 } from "../../helpers/interfaces"
+import { MapV1 } from "../../helpers/interfaces.tsx"
 
 export default function MatchCompare() {
   const [maps, setMaps] = useState<MapV1[] | null>(null)
--- othFrontend/src/services/othApiService.ts
+++ othFrontend/src/services/othApiService.ts
@@ -1,4 +1,3 @@
-import process from "node:process"
 import axios, { AxiosResponse, isAxiosError } from "axios"
 import toast from "react-hot-toast"
 import {
